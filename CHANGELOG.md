c5b0fd5cdf9dfc8d3372e76e82d2c9bed9927c34
Query Kuma <querykuma@users.noreply.github.com>
2020-07-25 04:11:55 +0900

    fix:top folder didn't work correctly, error check

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1] - 2020-07-25
+
+- Fix: top folder did not work correctly. add error check.
+
 ## [1.0.0] - 2020-07-24
 
 - New: add command "Add This Folder to Workspace & Close Others"
--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,19 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const get_folder = (filepath: string) => {
 		const slash = filepath.replace(/\\/gu, "/");
-		const folder = slash.replace(/\/[^/]*$/u, "");
+		let folder = slash.replace(/\/[^/]*$/u, "");
+		const m = folder.match(/^\/?\w:$/);
+		if (m) {
+			// top folder
+			folder = folder + "/";
+		}
 		return folder;
+	};
+
+	const is_folder_equal = (folder1: string, folder2: string): boolean => {
+		const f1 = folder1.replace(/^\/?(.*)\/?$/u, "$1");
+		const f2 = folder2.replace(/^\/?(.*)\/?$/u, "$1");
+		return f1.toLowerCase() === f2.toLowerCase();
 	};
 
 	let disposable = vscode.commands.registerCommand('add-this-folder-to-workspace.run', () => {
@@ -14,10 +25,13 @@ export function activate(context: vscode.ExtensionContext) {
 		if (!filepath) { return; }
 		const folder = get_folder(filepath);
 		const folder_uri = vscode.Uri.file(folder);
+
+		// console.log(vscode.workspace.workspaceFolders?.map(a => a.uri.path));
 		vscode.workspace.updateWorkspaceFolders(
 			vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders.length : 0,
 			null,
 			{ uri: folder_uri });
+
 	});
 
 	context.subscriptions.push(disposable);
@@ -29,10 +43,10 @@ export function activate(context: vscode.ExtensionContext) {
 		const folder = get_folder(filepath);
 		const folder_uri = vscode.Uri.file(folder);
 
-		if (vscode.workspace.workspaceFolders!.length) {
+		if (vscode.workspace.workspaceFolders?.length) {
 
 			if (vscode.workspace.workspaceFolders!.length === 1
-				&& vscode.workspace.workspaceFolders![0].uri.path === folder_uri.path) { return; }
+				&& is_folder_equal(vscode.workspace.workspaceFolders![0].uri.path, folder)) { return; }
 
 			let options: vscode.InputBoxOptions = {
 				prompt: "Add This Folder to Workspace & Remove Others from Workspace. OK?",
@@ -43,14 +57,25 @@ export function activate(context: vscode.ExtensionContext) {
 
 		}
 
-		vscode.workspace.updateWorkspaceFolders(
+		// console.log(vscode.workspace.workspaceFolders?.map(a => a.uri.path));
+		var ret = vscode.workspace.updateWorkspaceFolders(
 			0,
-			vscode.workspace.workspaceFolders!.length,
+			vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders.length : 0,
 			{ uri: folder_uri });
+
+		if (ret !== true) {
+			throw new Error("ret !== true");
+		}
+		if (vscode.workspace.workspaceFolders!.length !== 1) {
+			throw new Error("vscode.workspace.workspaceFolders!.length !== 1");
+		}
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+	console.log("deactivate");
+}
